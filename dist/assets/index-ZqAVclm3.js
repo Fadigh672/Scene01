eebbe76862541b6b1caecd5934ea42d6ab41ef94
Fadigh672 <151573513+Fadigh672@users.noreply.github.com>
2024-02-01 19:02:56 +0100

    commited

---
+++ dist/assets/index-ZqAVclm3.js
@@ -0,0 +1,5 @@
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();document.querySelector("#app").innerHTML=`<div class = 'info'>
+  <h1>Ghamoud Fadi Project</h1>
+  <p>TP01 using Spline Tool</p>
+</div>
+<iframe src='https://my.spline.design/blockycopy-0d7ec9bb1f78dc9f24847df15876c568/' frameborder='0'></iframe>`;
