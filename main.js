145b3bc4e3828cac1bb486397bf5904cb687634f
Fadigh672 <151573513+Fadigh672@users.noreply.github.com>
2024-02-01 17:00:46 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = 
+`<div class = 'info'>
+  <h1>Ghamoud Fadi Project</h1>
+  <p>TP01 using Spline Tool</p>
+</div>
+<iframe src='https://my.spline.design/blockycopy-0d7ec9bb1f78dc9f24847df15876c568/' frameborder='0'></iframe>`
